d5c8330d5f43028e1396b5b49917fb387e3b129d
Zain <bestf7593@gmail.com>
2025-09-04 01:38:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("alert alert-light");
+  const [colorName, setColorName] = useState("white");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 text-center">
+      <h1 className="color-dark bg-white mb-5">This is dinamic alert throught select options</h1>
+      <div className="container mb-5">
+        <select onChange={(e)=>{setColor(e.target.value); setColorName(e.target.options[e.target.selectedIndex].text)}} class="form-select" aria-label="Default select example">
+          <option selected>Change alert Color</option>
+          <option value="alert alert-danger">red</option>
+          <option value="alert alert-success">green</option>
+          <option value="alert alert-primary">blue</option>
+          <option value="alert alert-secondary">gray</option>
+          <option value="alert alert-warning">yellow</option>
+          <option value="alert alert-info">sea green</option>
+          <option value="alert alert-light">white</option>
+          <option value="alert alert-dark">blackS</option>
+        </select>
+      </div>
+    <div className={color} role="alert">
+      {colorName}
+    </div>
+    {/* <span className="badge text-bg-primary" onClick={()=>setColor(color + 1)}>Primary</span> */}
     </div>
   );
 }
